062732f35b84def45c1157826ecc9df8604f13af
Nir Katz <99401719+knir44@users.noreply.github.com>
2025-03-05 23:39:29 +0200

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SorryFaceBtn = new System.Windows.Forms.Button();
+            this.yesKingFaceBtn = new System.Windows.Forms.Button();
+            this.noKingFaceBtn = new System.Windows.Forms.Button();
+            this.MichalFaceBtn = new System.Windows.Forms.Button();
+            this.unknownFaceBtn = new System.Windows.Forms.Button();
+            this.pointsFaceBtn = new System.Windows.Forms.Button();
+            this.ConfusedFaceBtn = new System.Windows.Forms.Button();
+            this.noFaceBtn = new System.Windows.Forms.Button();
+            this.shockedFaceBtn = new System.Windows.Forms.Button();
+            this.bonusFaceBtn = new System.Windows.Forms.Button();
+            this.joyFaceBtn = new System.Windows.Forms.Button();
+            this.cryFaceBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SorryFaceBtn
+            // 
+            this.SorryFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.SorryFace;
+            this.SorryFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SorryFaceBtn.Location = new System.Drawing.Point(424, 442);
+            this.SorryFaceBtn.Name = "SorryFaceBtn";
+            this.SorryFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.SorryFaceBtn.TabIndex = 11;
+            this.SorryFaceBtn.UseVisualStyleBackColor = true;
+            this.SorryFaceBtn.Click += new System.EventHandler(this.SorryFaceBtn_Click);
+            // 
+            // yesKingFaceBtn
+            // 
+            this.yesKingFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.yesKingFace;
+            this.yesKingFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.yesKingFaceBtn.Location = new System.Drawing.Point(218, 441);
+            this.yesKingFaceBtn.Name = "yesKingFaceBtn";
+            this.yesKingFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.yesKingFaceBtn.TabIndex = 10;
+            this.yesKingFaceBtn.UseVisualStyleBackColor = true;
+            this.yesKingFaceBtn.Click += new System.EventHandler(this.yesKingFaceBtn_Click);
+            // 
+            // noKingFaceBtn
+            // 
+            this.noKingFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.noKingFace;
+            this.noKingFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.noKingFaceBtn.Location = new System.Drawing.Point(12, 441);
+            this.noKingFaceBtn.Name = "noKingFaceBtn";
+            this.noKingFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.noKingFaceBtn.TabIndex = 9;
+            this.noKingFaceBtn.UseVisualStyleBackColor = true;
+            this.noKingFaceBtn.Click += new System.EventHandler(this.noKingFaceBtn_Click);
+            // 
+            // MichalFaceBtn
+            // 
+            this.MichalFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.MichalFace;
+            this.MichalFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.MichalFaceBtn.Location = new System.Drawing.Point(424, 299);
+            this.MichalFaceBtn.Name = "MichalFaceBtn";
+            this.MichalFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.MichalFaceBtn.TabIndex = 8;
+            this.MichalFaceBtn.UseVisualStyleBackColor = true;
+            this.MichalFaceBtn.Click += new System.EventHandler(this.MichalFaceBtn_Click);
+            // 
+            // unknownFaceBtn
+            // 
+            this.unknownFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.unknownFace;
+            this.unknownFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.unknownFaceBtn.Location = new System.Drawing.Point(218, 298);
+            this.unknownFaceBtn.Name = "unknownFaceBtn";
+            this.unknownFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.unknownFaceBtn.TabIndex = 7;
+            this.unknownFaceBtn.UseVisualStyleBackColor = true;
+            this.unknownFaceBtn.Click += new System.EventHandler(this.unknownFaceBtn_Click);
+            // 
+            // pointsFaceBtn
+            // 
+            this.pointsFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.pointsFace;
+            this.pointsFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pointsFaceBtn.Location = new System.Drawing.Point(12, 298);
+            this.pointsFaceBtn.Name = "pointsFaceBtn";
+            this.pointsFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.pointsFaceBtn.TabIndex = 6;
+            this.pointsFaceBtn.UseVisualStyleBackColor = true;
+            this.pointsFaceBtn.Click += new System.EventHandler(this.pointsFaceBtn_Click);
+            // 
+            // ConfusedFaceBtn
+            // 
+            this.ConfusedFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.ConfusedFace;
+            this.ConfusedFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ConfusedFaceBtn.Location = new System.Drawing.Point(424, 156);
+            this.ConfusedFaceBtn.Name = "ConfusedFaceBtn";
+            this.ConfusedFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.ConfusedFaceBtn.TabIndex = 5;
+            this.ConfusedFaceBtn.UseVisualStyleBackColor = true;
+            this.ConfusedFaceBtn.Click += new System.EventHandler(this.ConfusedFaceBtn_Click);
+            // 
+            // noFaceBtn
+            // 
+            this.noFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.noFace;
+            this.noFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.noFaceBtn.Location = new System.Drawing.Point(218, 156);
+            this.noFaceBtn.Name = "noFaceBtn";
+            this.noFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.noFaceBtn.TabIndex = 4;
+            this.noFaceBtn.UseVisualStyleBackColor = true;
+            this.noFaceBtn.Click += new System.EventHandler(this.noFace_Click);
+            // 
+            // shockedFaceBtn
+            // 
+            this.shockedFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.shockedFace;
+            this.shockedFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.shockedFaceBtn.Location = new System.Drawing.Point(12, 155);
+            this.shockedFaceBtn.Name = "shockedFaceBtn";
+            this.shockedFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.shockedFaceBtn.TabIndex = 3;
+            this.shockedFaceBtn.UseVisualStyleBackColor = true;
+            this.shockedFaceBtn.Click += new System.EventHandler(this.shockedFaceBtn_Click);
+            // 
+            // bonusFaceBtn
+            // 
+            this.bonusFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.bonusFace;
+            this.bonusFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bonusFaceBtn.Location = new System.Drawing.Point(424, 12);
+            this.bonusFaceBtn.Name = "bonusFaceBtn";
+            this.bonusFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.bonusFaceBtn.TabIndex = 2;
+            this.bonusFaceBtn.UseVisualStyleBackColor = true;
+            this.bonusFaceBtn.Click += new System.EventHandler(this.bonusFaceBtn_Click);
+            // 
+            // joyFaceBtn
+            // 
+            this.joyFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.joyFace;
+            this.joyFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.joyFaceBtn.Location = new System.Drawing.Point(218, 12);
+            this.joyFaceBtn.Name = "joyFaceBtn";
+            this.joyFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.joyFaceBtn.TabIndex = 1;
+            this.joyFaceBtn.UseVisualStyleBackColor = true;
+            this.joyFaceBtn.Click += new System.EventHandler(this.joyFaceBtn_Click);
+            // 
+            // cryFaceBtn
+            // 
+            this.cryFaceBtn.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.cryFace;
+            this.cryFaceBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cryFaceBtn.Location = new System.Drawing.Point(12, 12);
+            this.cryFaceBtn.Name = "cryFaceBtn";
+            this.cryFaceBtn.Size = new System.Drawing.Size(189, 137);
+            this.cryFaceBtn.TabIndex = 0;
+            this.cryFaceBtn.UseVisualStyleBackColor = true;
+            this.cryFaceBtn.Click += new System.EventHandler(this.cryFaceBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 603);
+            this.Controls.Add(this.SorryFaceBtn);
+            this.Controls.Add(this.yesKingFaceBtn);
+            this.Controls.Add(this.noKingFaceBtn);
+            this.Controls.Add(this.MichalFaceBtn);
+            this.Controls.Add(this.unknownFaceBtn);
+            this.Controls.Add(this.pointsFaceBtn);
+            this.Controls.Add(this.ConfusedFaceBtn);
+            this.Controls.Add(this.noFaceBtn);
+            this.Controls.Add(this.shockedFaceBtn);
+            this.Controls.Add(this.bonusFaceBtn);
+            this.Controls.Add(this.joyFaceBtn);
+            this.Controls.Add(this.cryFaceBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TheBalum";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cryFaceBtn;
+        private System.Windows.Forms.Button joyFaceBtn;
+        private System.Windows.Forms.Button bonusFaceBtn;
+        private System.Windows.Forms.Button shockedFaceBtn;
+        private System.Windows.Forms.Button noFaceBtn;
+        private System.Windows.Forms.Button ConfusedFaceBtn;
+        private System.Windows.Forms.Button pointsFaceBtn;
+        private System.Windows.Forms.Button unknownFaceBtn;
+        private System.Windows.Forms.Button MichalFaceBtn;
+        private System.Windows.Forms.Button noKingFaceBtn;
+        private System.Windows.Forms.Button yesKingFaceBtn;
+        private System.Windows.Forms.Button SorryFaceBtn;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,174 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, WaveOutEvent> waveOutDict = new Dictionary<string, WaveOutEvent>(); 
+        private Dictionary<string, AudioFileReader> audioFileDict = new Dictionary<string, AudioFileReader>(); 
+        private Dictionary<string, bool> isPlayingDict = new Dictionary<string, bool>(); 
+
+        public Form1()
+        {
+            InitializeComponent();
+            isPlayingDict.Add("cryFaceBtn", false);
+            isPlayingDict.Add("joyFaceBtn", false);
+            isPlayingDict.Add("bonusFaceBtn", false);
+            isPlayingDict.Add("shockedFaceBtn", false);
+            isPlayingDict.Add("noFaceBtn", false);
+            isPlayingDict.Add("ConfusedFaceBtn", false);
+
+            isPlayingDict.Add("pointsFaceBtn", false);
+            isPlayingDict.Add("unknownFaceBtn", false);
+            isPlayingDict.Add("MichalFaceBtn", false);
+            isPlayingDict.Add("noKingFaceBtn", false);
+            isPlayingDict.Add("yesKingFaceBtn", false);
+            isPlayingDict.Add("SorryFaceBtn", false);
+        }
+
+        private void PlayOrStopAudio(string buttonName, string filePath)
+        {
+            try
+            {
+                if (!isPlayingDict[buttonName])
+                {
+                    foreach (var key in isPlayingDict.Keys)
+                    {
+                        if (key != buttonName && isPlayingDict[key])
+                        {
+                            StopAudio(key); 
+                        }
+                    }
+
+                    PlayAudio(buttonName, filePath);
+                    isPlayingDict[buttonName] = true;
+                }
+                else 
+                {
+                    StopAudio(buttonName);
+                    isPlayingDict[buttonName] = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void PlayAudio(string buttonName, string filePath)
+        {
+            try
+            {
+                if (waveOutDict.ContainsKey(buttonName)) 
+                {
+                    StopAudio(buttonName);
+                }
+
+                var audioFile = new AudioFileReader(filePath);
+                var waveOut = new WaveOutEvent();
+                waveOut.Init(audioFile);
+                waveOut.Play();
+
+                waveOut.PlaybackStopped += (s, ev) =>
+                {
+                    isPlayingDict[buttonName] = false; 
+                };
+
+                waveOutDict[buttonName] = waveOut;
+                audioFileDict[buttonName] = audioFile;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error playing the file: " + ex.Message);
+            }
+        }
+
+        private void StopAudio(string buttonName)
+        {
+            try
+            {
+                if (waveOutDict.ContainsKey(buttonName))
+                {
+                    waveOutDict[buttonName].Stop();
+                    waveOutDict[buttonName].Dispose();
+                    waveOutDict.Remove(buttonName);
+                }
+
+                if (audioFileDict.ContainsKey(buttonName))
+                {
+                    audioFileDict[buttonName].Dispose();
+                    audioFileDict.Remove(buttonName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error stopping the music: " + ex.Message);
+            }
+        }
+
+        private void cryFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("cryFaceBtn", "cryVoice.Wav");
+        }
+
+        private void joyFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("joyFaceBtn", "joyVoice.Wav");
+        }
+
+        private void bonusFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("bonusFaceBtn", "bonusVoice.Wav");
+        }
+
+        private void shockedFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("shockedFaceBtn", "shockedVoice.Wav");
+        }
+
+        private void noFace_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("noKingFaceBtn", "noVoice.Wav");
+        }
+
+        private void ConfusedFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("yesKingFaceBtn", "yesKingVoice.Wav");
+        }
+
+        private void pointsFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("pointsFaceBtn", "PointVoice.Wav");
+
+        }
+
+        private void unknownFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("unknownFaceBtn", "unknownVoice.Wav");
+        }
+
+        private void MichalFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("MichalFaceBtn", "MichalVoice.Wav");
+        }
+
+        private void noKingFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("noKingFaceBtn", "noKingVoice.Wav");
+        }
+
+        private void yesKingFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("yesKingFaceBtn", "yesKingVoice.Wav");
+
+        }
+
+        private void SorryFaceBtn_Click(object sender, EventArgs e)
+        {
+            PlayOrStopAudio("SorryFaceBtn", "SorryVoice.Wav");
+        }
+    }
+}
